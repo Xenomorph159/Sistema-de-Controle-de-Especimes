4849532ac0dd116a4d94ae27863eb73fc41c38d3
Isadora Tatiane Soares <119127102+Xenomorph159@users.noreply.github.com>
2023-09-03 23:31:49 -0300

    Delete src directory

--- src/Classes/DAO.java
+++
@@ -1,9 +0,0 @@
-package Classes;
-
-public interface DAO {
-    public boolean adicionar(int cod, Object objeto);
-    public boolean remover(int cod);
-    public String pesquisar(int cod);
-    public Object listar();
-    
-}
